3a77d263bdd8a3bcc5deadef00950bdc5ac690c9
Rebeca Mello <rebecammsilva@outlook.com>
2025-02-19 20:30:56 -0300

    :tada: Estrutura inicial do projeto

---
+++ frontend/financas/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <h1>Finanças</h1>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App',
+
+  components: {
+    //
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
+
+<style>
+
+</style>
